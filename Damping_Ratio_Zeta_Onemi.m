a42b3a8f5928a3992219b6e3374ec1eb1a3b90f7
Turhan Can Kargın <turhancan.kargin@gmail.com>
2020-06-25 19:38:49 +0300

    Add files via upload

--- Damping_Ratio_Zeta_Onemi.m
+++ Damping_Ratio_Zeta_Onemi.m
@@ -1,5 +1,11 @@
-zeta=0.3;
+% Observing Damping Ratio Variation
+
 wn=1;
+zeta=2;
+y=tf([wn],[1 2*zeta*wn wn]);
+step(y);
+hold on
+zeta=1;
 y=tf([wn],[1 2*zeta*wn wn]);
 step(y);
 hold on
@@ -7,7 +13,16 @@ zeta=0.5;
 y=tf([wn],[1 2*zeta*wn wn]);
 step(y);
 hold on
+zeta=0.3;
+y=tf([wn],[1 2*zeta*wn wn]);
+step(y);
+hold on
 zeta=0.1;
 y=tf([wn],[1 2*zeta*wn wn]);
 step(y);
-legend('zeta=0.3','zeta=0.5','zeta=0.1','zeta=0')
+hold on
+zeta=0.05;
+y=tf([wn],[1 2*zeta*wn wn]);
+step(y);
+legend('zeta=2','zeta=1','zeta=0.5','zeta=0.3','zeta=0.1','zeta=0.05')
+title('Importance of Damping Ratio')
--- Natural_Frequency_Wn_Onemi.m
+++ Natural_Frequency_Wn_Onemi.m
@@ -1,15 +1,23 @@
-zeta=0.3;
+% Observing Natural Frequency Variation
+
+zeta=0.5;
+wn=0.5;
+y=tf([wn],[1 2*zeta*wn wn]);
+step(y);
+hold on
+zeta=0.5;
 wn=1;
 y=tf([wn],[1 2*zeta*wn wn]);
 step(y);
 hold on
-zeta=0.3;
+zeta=0.5;
 wn=1.5;
 y=tf([wn],[1 2*zeta*wn wn]);
 step(y);
 hold on
-zeta=0.3;
+zeta=0.5;
 wn=5;
 y=tf([wn],[1 2*zeta*wn wn]);
 step(y);
-legend('wn=1','wn=1.5','wn=5')
+legend('wn=0.5','wn=1','wn=1.5','wn=5')
+title('Importance of Natural Frequency')
